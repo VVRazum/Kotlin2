4ad9676eb8ca0c39270c9951e6dc37c718294ee1
VVRazum <ky3@list.ru>
2024-05-31 19:30:44 +0300

    2-3

---
+++ src/main/kotlin/Lesson2/Lesson2_task4.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    val crystal = 7
+    val metall = 11
+    val crystalName = "Buff по кристалам"
+    val metallName = "Buff по железу"
+
+    val crystalVsBuff = crystal * 20 / 100
+    val metallVSBuff = metall * 20 / 100
+
+    println("$crystalName:$crystalVsBuff")
+    println("$metallName:$metallVSBuff")
+
+}
+
