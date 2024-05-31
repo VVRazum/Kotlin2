ef00f4c02fa72923599c7f24264240e4dc639e02
VVRazum <ky3@list.ru>
2024-05-31 19:37:46 +0300

    3-2

---
+++ src/main/kotlin/Lesson3/Lesson3_task2.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val namePrimary = "Татьяна"
+    val secondNamePrimary = "Сергеевна"
+    val surnamePrimary = "Андреева"
+    val agePrimary = 20
+    val surnameSecondary = "Сидорова"
+    val ageSecondary = 22
+
+    println("$surnamePrimary $namePrimary $secondNamePrimary, $agePrimary")
+    println("$surnameSecondary $namePrimary $secondNamePrimary, $ageSecondary")
+}
