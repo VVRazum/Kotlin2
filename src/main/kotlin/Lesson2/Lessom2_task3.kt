8d91540a0dd2935dff9909907290e678f4ea1474
VVRazum <ky3@list.ru>
2024-05-31 19:28:22 +0300

    2-3

---
+++ src/main/kotlin/Lesson2/Lessom2_task3.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val startTotalMinutes = 9 * 60 + 39
+    val finishTotalMinutes = startTotalMinutes + 457
+    val finishHours = finishTotalMinutes / 60
+    val finishMinutes = finishTotalMinutes % 60
+
+    println("$finishHours:$finishMinutes")
+    //Данную историю пришлось гуглить. В твоей подаче в теории на ютубе этого нет. Не понятно, как решить по другому. Возможно тут другой вариант можно использовать
+}
+
