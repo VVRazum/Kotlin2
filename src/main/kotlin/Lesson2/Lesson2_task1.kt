64c227b3ec5c16c65c02ef4c0b5f25f32cfc61d7
VVRazum <ky3@list.ru>
2024-05-31 19:17:52 +0300

    Add Category

---
+++ src/main/kotlin/Lesson2/Lesson2_task1.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val studentA = 3
+    val studentB = 4
+    val studentC = 3
+    val studentD = 5
+
+    val summary: Float = (studentA + studentB + studentC + studentD) / 4f
+
+    println(summary)
+}
