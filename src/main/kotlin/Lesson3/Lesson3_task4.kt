bb0b12446580d40f23982cb2ae8fbe7f3047242d
VVRazum <ky3@list.ru>
2024-05-31 19:53:07 +0300

    3-4

---
+++ src/main/kotlin/Lesson3/Lesson3_task4.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+//Ходят чертные
+    var startPos = "E2"
+    var finishPos = "E4"
+    var numMove = 1
+
+    var sumMove = ("$startPos-$finishPos;$numMove")
+    println(sumMove)
+
+//Ходят белые
+    startPos = "D2"
+    finishPos = "D3"
+    numMove = numMove + 1
+
+    sumMove = """$startPos-$finishPos;$numMove"""
+    println(sumMove)
+
+}
