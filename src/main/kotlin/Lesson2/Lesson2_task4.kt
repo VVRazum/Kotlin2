e8026ae3f9f4534055502542065f7183da21cf3e
VVRazum <ky3@list.ru>
2024-06-03 16:51:43 +0300

    2-4 adding variable buffPercent

--- src/main/kotlin/Lesson2/Lesson2_task4.kt
+++ src/main/kotlin/Lesson2/Lesson2_task4.kt
@@ -4,9 +4,10 @@ fun main() {
     val metall = 11
     val crystalName = "Buff по кристалам"
     val metallName = "Buff по железу"
+    val buffPercent = 20
 
-    val crystalVsBuff = crystal * 20 / 100
-    val metallVSBuff = metall * 20 / 100
+    val crystalVsBuff = crystal * buffPercent/ 100
+    val metallVSBuff = metall * buffPercent / 100
 
     println("$crystalName:$crystalVsBuff")
     println("$metallName:$metallVSBuff")
