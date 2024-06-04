bef72200016415ad0dd8d97dc63090bcca2a6152
VVRazum <ky3@list.ru>
2024-06-04 12:26:46 +0300

    3-5 Parsing

---
+++ src/main/kotlin/Lesson3/Lesson3_task5.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    val someStep = "D2-D4;0"
+
+    val stepPars = someStep.split("-", ";")
+    //println(stepPars)
+
+    val (startStep,finishStep,numberStep) = Triple(stepPars[0], stepPars[1], stepPars[2])
+    println("$startStep\n$finishStep\n$numberStep")
+
+}
+
+
