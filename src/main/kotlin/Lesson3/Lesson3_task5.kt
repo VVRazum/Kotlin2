76eb2cf98fb76b778fa062058dfdca57a8d2edd7
VVRazum <ky3@list.ru>
2024-06-04 18:17:12 +0300

    3-5

--- src/main/kotlin/Lesson3/Lesson3_task5.kt
+++ src/main/kotlin/Lesson3/Lesson3_task5.kt
@@ -2,7 +2,9 @@ fun main() {
 
     val someStep = "D2-D4;0"
     val stepPars = someStep.split("-", ";")
-    val (startStep,finishStep,numberStep) = Triple(stepPars[0], stepPars[1], stepPars[2])
+    val startStep = stepPars[0]
+    val finishStep = stepPars[1]
+    val numberStep = stepPars[2]
 
     println("$startStep\n$finishStep\n$numberStep")
 
