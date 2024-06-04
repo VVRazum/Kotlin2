f5494cecba99fc457720c3e6769f0e0f4349624b
VVRazum <ky3@list.ru>
2024-06-04 19:04:06 +0300

    3-3 Mechanical numbers

--- src/main/kotlin/Lesson3/Lesson3_Task3.kt
+++ src/main/kotlin/Lesson3/Lesson3_Task3.kt
@@ -1,11 +1,20 @@
 fun main() {
 
-    println("Введите число:")
-    val number = Integer.valueOf(readln())
+    val startNumber = 2
 
-    for (x in 1..9) {
-        println("$number * $x = " + (number * x))
-    }
+    val one = startNumber * 1
+    val two = startNumber * 2
+    val three = startNumber * 3
+    val four = startNumber * 4
+    val five = startNumber * 5
+    val six = startNumber * 6
+    val seven = startNumber * 7
+    val eight = startNumber * 8
+    val nine = startNumber * 9
+
+    println("$startNumber x 1 = $one\n$startNumber x 2 = $two\n$startNumber x 3 = $three\n$startNumber x 4 = $four\n$startNumber x 5 = $five\n" +
+            "$startNumber x 6 = $six\n$startNumber x 7 = $seven\n$startNumber x 8 = $eight\n$startNumber x 9 = $nine")
+
 }
 
 
